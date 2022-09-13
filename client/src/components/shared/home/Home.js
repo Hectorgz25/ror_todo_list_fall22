2d7eedc3fc0e0870b6c319b18e743f917bdb172b
Hector Gonzalez <hectorgz25@icloud.com>
2022-09-12 21:01:17 -0600

    front end add, index, show, edit

---
+++ client/src/components/shared/home/Home.js
@@ -0,0 +1,14 @@
+
+import HomeHeader from "./HomeHeader";
+import Features from "./Features";
+import Price from "./Price";
+
+const Home = () => (
+  <>
+    <HomeHeader />
+    <Features />
+    <Price />
+  </>
+)
+
+export default Home;
